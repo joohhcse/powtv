b3cec3e8af264760ded8075053c19019aba72b25
hhjoo <joohhcse@gmail.com>
2024-09-19 17:31:49 +0900

    powtv init

---
+++ src/main/java/com/jooplayconsole/powtv/PowtvApplication.java
@@ -0,0 +1,13 @@
+package com.jooplayconsole.powtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowtvApplication.class, args);
+	}
+
+}
